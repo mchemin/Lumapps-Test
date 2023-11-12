ea8bbf8decdbe1d6318d38bafb2b07c5d554af25
matthieuchemin <matthieu.chemin@finalcad.com>
2023-11-12 22:46:30 +0100

    Improve user experience for refresh when seeing user detail

--- app/src/main/java/com/chemin/lumappstest/domain/usecase/GetUserDetailInfo.kt
+++ app/src/main/java/com/chemin/lumappstest/domain/usecase/GetUserDetailInfo.kt
@@ -7,6 +7,7 @@ import com.chemin.lumappstest.domain.model.UserId
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 
 interface GetUserDetail {
@@ -22,6 +23,7 @@ class GetUserDetailImpl(
     }
 
     override fun invoke(userId: UserId?): Flow<UserDetailInfoState> {
+        var lastUserDetailInfo: UserDetailInfo? = null
         val flow = userId?.let { id ->
             usersRepository
                 .getUserDetail(userId = id)
@@ -42,5 +44,22 @@ class GetUserDetailImpl(
         } ?: flowOf(UserDetailInfoState.ErrorEmptyId)
         return flow
             .onStart { emit(UserDetailInfoState.Loading) }
+            // we keep track of the last user info we get
+            .onEach { userDetailInfoState ->
+                if (userDetailInfoState is UserDetailInfoState.Success) {
+                    lastUserDetailInfo = userDetailInfoState.userDetailInfo
+                }
+            }
+            // if the user become missing, we replace it by its last representation if existing
+            .map { userDetailInfoState ->
+                lastUserDetailInfo?.let { userDetailInfo ->
+                    if (userDetailInfoState is UserDetailInfoState.ErrorMissingUserInfo) {
+                        UserDetailInfoState.Success(userDetailInfo)
+                    } else {
+                        userDetailInfoState
+                    }
+                } ?: userDetailInfoState
+            }
+
     }
 }
